41a306fa1bc3d7faf071009d5806a1b2cf268ad0
shakespearea <shakespeareamber@gmail.com>
2024-02-17 18:49:43 +0100

    [add] Ability to request specific songs

--- source/lyrics_scraper.py
+++ source/lyrics_scraper.py
@@ -2,49 +2,40 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-import re
 
 # Get the Genius API key from the environment variable
 load_dotenv("source\GENIUS_API_KEY.env")
 genius_api_key = os.getenv("GENIUS_API_KEY")
 
-# Get artist object from Genius API
-def request_artist_info(artist_name, page):
+# Get song object from Genius API
+def request_song_info(song_title, page):
     base_url = 'https://api.genius.com'
     headers = {'Authorization': 'Bearer ' + genius_api_key}
     search_url = base_url + '/search?per_page=10&page=' + str(page)
-    data = {'q': artist_name}
+    data = {'q': song_title}
     response = requests.get(search_url, data=data, headers=headers)
     return response
 
-# Get Genius.com song url's from artist object
-def request_song_url(artist_name, song_cap):
-    page = 1
-    songs = []
-    
-    while True:
-        response = request_artist_info(artist_name, page)
+def request_song_url(song_title, artist_name, page_cap):
+    for i in range(1, page_cap):
+        page = i
+
+        response = request_song_info(song_title, page)
         json = response.json()
-        # Collect up to song_cap song objects from artist
-        song_info = []
+
+        # Exit if no results found
+        if not json['response']['hits']:
+            print('No songs found with the title "{}" by {}'.format(song_title, artist_name))
+            return None
+
+        # Song must match the song title and artist
         for hit in json['response']['hits']:
-            if artist_name.lower() in hit['result']['primary_artist']['name'].lower():
-                song_info.append(hit)
-    
-        # Collect song URL's from song objects
-        for song in song_info:
-            if (len(songs) < song_cap):
-                url = song['result']['url']
-                songs.append(url)
-            
-        if (len(songs) == song_cap):
-            break
-        else:
-            page += 1
-        
-    print('Found {} songs by {}'.format(len(songs), artist_name))
-    return songs
-    
+            if (song_title.lower() in hit['result']['title'].lower() and
+                    artist_name.lower() in hit['result']['primary_artist']['name'].lower()):
+                return hit['result']['url']
+
+    print('No songs found with the title "{}" by {}'.format(song_title, artist_name))
+
 # Get lyrics from specified Genius.com song URL
 def scrape_song_lyrics(url):
     page = BeautifulSoup(requests.get(url).content, 'lxml')
@@ -57,5 +48,7 @@ def scrape_song_lyrics(url):
 
     return True
 
-# Get first 10 results from Frank Sinatra and return lyrics to the first
-scrape_song_lyrics(request_song_url('Frank Sinatra', 10)[0])
+# Search lyrics for 'Fly me to the Moon' by Frank Sinatra within 5 pages of search results.
+song_url = request_song_url('Fly me to the Moon', 'Frank Sinatra', 5)
+if song_url:
+    scrape_song_lyrics(song_url)
